29e94be1b2dd8b89b20df295fbeb43c40f20d1e6
Jonathan Salazar <jonathan96@unitec.edu>
2019-03-22 20:19:36 -0600

    Added TopBar.

--- src/App.js
+++ src/App.js
@@ -1,57 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    height: 140,
-    width: 100,
-  },
-  control: {
-    padding: theme.spacing.unit * 2,
-  },
-});
+import TopBar from './components/topBar';
+import Layout from './components/layout';
 
 class App extends React.Component {
-  state = {
-    spacing: '16',
-  };
-
   render() {
-    const { classes } = this.props;
 
     return (
-		<Grid container className={classes.root} spacing={16}>
-			<Grid item xs={12}>
-				<Paper className={classes.control}>
-					<Grid container>
-						<Grid item>
-							Image will go here
-						</Grid>
-					</Grid>
-				</Paper>
-			</Grid>
-			<Grid item xs={12}>
-				<Paper className={classes.control}>
-					<Grid container>
-						<Grid item>
-							Pixelation options will go here
-						</Grid>
-					</Grid>
-				</Paper>
-			</Grid>
-      	</Grid>
+		<div>
+			<TopBar />
+			<Layout />
+		</div>	
     );
   }
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(App);
+export default App;
---
+++ src/components/layout.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
+
+class Layout extends React.Component {
+  state = {
+    spacing: '16',
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+		<div>
+			<Grid container className={classes.root} spacing={16}>
+				<Grid item xs={12}>
+					<Paper className={classes.control}>
+						<Grid container>
+							<Grid item>
+								Image will go here
+							</Grid>
+						</Grid>
+					</Paper>
+				</Grid>
+				<Grid item xs={12}>
+					<Paper className={classes.control}>
+						<Grid container>
+							<Grid item>
+								Pixelation options will go here
+							</Grid>
+						</Grid>
+					</Paper>
+				</Grid>
+			</Grid>
+		</div>
+		
+    );
+  }
+}
+
+Layout.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Layout);
