de3193e70509e41419675a6d3d49de95a94df64b
Jonathan Salazar <jonathan96@unitec.edu>
2019-03-26 10:50:40 -0600

    Added API_URL for requests

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const PixelateImage = (image64) => {
-	return axios.post(`http://127.0.0.1:4000/api/Pixelates/pixelate`, {image64: image64, pixelSize: 20, area: undefined})
+	return axios.post(`${process.env.API_URL}/Pixelates/pixelate`, {image64: image64, pixelSize: 20, area: undefined})
 	.then((response) => {
 		return response.data;
 	})
