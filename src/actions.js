e9c37622c556b2c3557299ce8d5275431788e4bd
Jonathan Salazar <jonathan96@unitec.edu>
2019-03-26 13:18:06 -0600

    Fixed HTTPS request link.

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const PixelateImage = (image64) => {
-	return axios.post(`${process.env.API_URL}/api/Pixelates/pixelate`, {image64: image64, pixelSize: 20, area: undefined})
+	return axios.post(`Pixelates/pixelate`, {image64: image64, pixelSize: 20, area: undefined})
 	.then((response) => {
 		return response.data;
 	})
